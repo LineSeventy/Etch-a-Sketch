d372f979af33fde81aa718f79adee4469b42fdd1
LineSeventy <169619934+LineSeventy@users.noreply.github.com>
2024-08-06 02:50:30 +0800

    Added trail hover effect

--- Script/script.js
+++ Script/script.js
@@ -1,6 +1,15 @@
+function random(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
 for (let x = 0; x < 256; x++) {
     let parentDiv = document.querySelector("#container");
     let grid = document.createElement("div");
     grid.className = "grid-item";
+    grid.addEventListener("mouseover", () => {
+        const rndColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+        grid.style.backgroundColor = rndColor;
+    });
+     
     parentDiv.appendChild(grid);
 }
+
